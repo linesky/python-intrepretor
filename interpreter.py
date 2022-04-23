593c1f7007c5525f2aa745762f0ac5fd3f3b4b2f
sky line <32512284+linesky@users.noreply.github.com>
2022-04-23 13:57:28 +0100

    python intrepreter.py run.shell

---
+++ interpreter.py
@@ -0,0 +1,22 @@
+import sys
+import os
+def srun(data):
+	os.system(data)
+		
+def scommand(data):
+	datas=data.split(";")
+	for n in datas:
+		srun(n)
+	
+def sline(argvs):
+	f1= open (argvs,"r+")
+	for n in f1:
+		if n!="":
+			if n!="\n":
+				scommand(n)
+				
+	f1.close()
+	
+	
+if sys.argv[1]!="":
+	sline(sys.argv[1])
